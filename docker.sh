be84f23d9d0b674928ee042efc6fad1bb6cbace5
thecatvoid <voidcat@tutanota.com>
2023-05-14 20:07:21 +0530

    Initial commit

---
+++ docker.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -e
+chroot="${HOME}/gentoo"
+
+unmount() {
+        mount | grep "$HOME/gentoo" | awk '{print $3}' |
+                while read -r i
+                do
+                        umount -Rf "$i" > /dev/null 2>&1 || true
+                done
+}
+
+rootch() {
+        sudo mount --rbind /dev "${chroot}/dev"
+        sudo mount --make-rslave "${chroot}/dev"
+        sudo mount -t proc /proc "${chroot}/proc"
+        sudo mount --rbind /sys "${chroot}/sys"
+        sudo mount --make-rslave "${chroot}/sys"
+        sudo mount --rbind /tmp "${chroot}/tmp"
+        sudo mount --bind /run "${chroot}/run"
+        sudo cp -L /etc/resolv.conf "${chroot}/etc/"
+        sudo cp -a ./* "${chroot}/root/"
+        sudo chroot "${chroot}" /root/gentoo.sh "$@"
+}
+
+bashin() {
+        rootch /bin/bash
+}
+
+setup_chroot() {
+        url="https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/"
+        file="$(curl -s "$url" | grep -Eo 'href=".*"' | awk -F '>' '{print $1}' |
+                sed -e 's/href=//g' -e 's/"//g' | grep -o "stage3-amd64-desktop-systemd-$(date +%Y).*.tar.xz" | uniq)"
+
+        curl -sSL "${url}${file}" -o "/var/tmp/${file}"
+        mkdir "$chroot"
+        sudo tar -C "${chroot}" -xpf "/var/tmp/${file}" --xattrs-include='*.*' --numeric-owner 2>/dev/null
+}
+
+setup_build_cmd() {
+        cd "$HOME" || exit
+        rm -rf /etc/portage/
+        emerge-webrsync
+        cp -af "${HOME}/portage" /etc/
+        sed -i "s/^J=.*/J=\"$(nproc --all)\"/" /etc/portage/make.conf
+        ln -sf /var/db/repos/gentoo/profiles/default/linux/amd64/17.1/desktop/systemd /etc/portage/make.profile
+        emerge dev-vcs/git app-accessibility/at-spi2-core
+        rm -rf /var/db/repos/* 
+        emerge --sync
+}
+
+build_cmd() {
+        emerge -uDN --with-bdeps=y @world || exit 1
+}
+
+clean_cmd() {
+        rm -rf /var/cache/
+}
+
+upload() {
+        root="${HOME}/gentoo"
+        XZ_OPTS="-T0" tar cJf "${root}.tar.xz" "${root}/*"
+        docker import "${root}.tar.xz" thecatvoid/gentoo:latest
+        docker login -u thecatvoid -p "$PASS"
+        docker push thecatvoid/gentoo:latest
+}
+
+# We got to do exec function inside gentoo chroot not on runner
+setup_build() {
+        rootch setup_build_cmd
+}
+
+build() {
+        rootch build_cmd
+}
+
+clean() {
+        rootch clean_cmd
+}
+
+# Exec functions when called as args
+for cmd; do $cmd; done
+trap 'unmount' EXIT
