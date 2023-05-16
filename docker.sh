baf2b43540b1fe9fe140329a4c4d019d48bb6e0b
void <92902636+thecatvoid@users.noreply.github.com>
2023-05-16 09:08:21 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -54,10 +54,31 @@ build_cmd() {
         emerge -uDN --with-bdeps=y @world || exit 1
 }
 
-compress() {
+cleanup() {
         unmount
         root="${HOME}/gentoo"
+        echo "will cite" | parallel --citation
+        parallel --use-cpus-instead-of-cores sudo rm -rf -- {} 2>/dev/null ::: /opt/hostedtoolcache ::: /usr/local/lib/android ::: /usr/share/gradle* /usr/bin/gradle /usr/share/apache-maven* /usr/bin/mvn
+
+parallel --use-cpus-instead-of-cores sudo rm -rf -- {} 2>/dev/null ::: /usr/local/share/vcpkg /usr/local/bin/vcpkg ::: /usr/share/miniconda ::: /usr/bin/conda /usr/local/lib/lein /usr/local/bin/lein \
+/usr/local/bin/pulumi* /usr/share/java/selenium-server-standalone.jar /usr/local/share/phantomjs* \
+/usr/local/bin/phantomjs /usr/local/share/chrome_driver /usr/bin/chromedriver /usr/local/share/gecko_driver \
+/usr/bin/geckodriver /etc/php /usr/bin/composer /usr/local/bin/phpunit /var/lib/mysql /etc/mysql \
+/usr/local/bin/sqlcmd /usr/local/bin/bcp /usr/local/bin/session-manager-plugin \
+/usr/local/julia* /usr/bin/julia /usr/share/rust /home/runner/.cargo /home/runner/.rustup \
+/home/runner/.ghcup /usr/local/bin/rake /usr/local/bin/rdoc /usr/local/bin/ri /usr/local/bin/racc \
+/usr/local/bin/rougify /usr/local/bin/bundle /usr/local/bin/bundler /var/lib/gems /usr/share/swift /usr/local/bin/swift /usr/local/bin/swiftc /usr/bin/ghc /usr/local/.ghcup /usr/local/bin/stack \
+/usr/local/bin/rebar3 /usr/share/sbt /usr/bin/sbt /usr/bin/go /usr/bin/gofmt /usr/local/bin/aws /usr/local/bin/aws_completer /usr/local/aws-cli /usr/local/aws /usr/local/bin/aliyun /usr/share/az_* \
+/opt/az /usr/bin/az /usr/local/bin/azcopy* /usr/bin/azcopy /usr/lib/azcopy /usr/local/bin/oc /usr/local/bin/oras /usr/local/bin/packer /usr/local/bin/terraform /usr/local/bin/helm /usr/local/bin/kubectl \
+/usr/local/bin/kind /usr/local/bin/kustomize /usr/local/bin/minikube /usr/libexec/catatonit/catatonit \
+/usr/share/dotnet /usr/local/graalvm /usr/share/man /var/lib/apt/lists/* /var/cache/apt/archives/*
+
         sudo rm -rf "${root}"{/var/cache/,/var/tmp/portage/,/tmp/portage/,/var/db/repos/}
+        docker rmi -f $(docker images -q) &>/dev/null
+}
+        
+compress() {
+    
         sudo tar cpf "${root}.tar" "$root" --strip-components=1 --xattrs-include='*.*' --numeric-owner || true
         pigz -cf -p "$(nproc --all)" "${root}.tar" > "${root}.tar.gz"
 
